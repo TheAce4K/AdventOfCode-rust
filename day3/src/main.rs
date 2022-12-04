d83d6fa4c46a884a4ab87cf7f5e34a723da16c24
Jesper Wistrom <jesper.wistrom@gmail.com>
2022-12-04 19:16:12 +0100

    Changed and simplified the solution a lot

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,61 +1,32 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
+use std::fs::read_to_string;
 
 
-fn read_data (filename: &str) -> String {
-    let file = File::open(filename).expect("File not found");
-    let mut data = String::new();
-    let mut buf_reader = BufReader::new(file);
-    buf_reader.read_to_string(&mut data).expect("Error reading file");
-    return data;
+fn find_value(c: char) -> u32 {
+    match c {
+        c if c.is_uppercase() => 26+c.to_digit(36).unwrap()-9,
+        c if c.is_lowercase() => c.to_digit(36).unwrap()-9,
+        _ => 0,
+    }
 }
 
+fn part1(input: &String) -> u32 {
+    input.split_terminator("\n").map(|line| {
+        let matched_char: char = line[0..line.len()].chars().find(|c| line[line.len()/2..].contains(*c)).unwrap();
+        find_value(matched_char) 
+    }).sum::<u32>()
+}
+fn part2(input: &String) -> u32 {
+    let lines: Vec<&str> = input.split_terminator("\n").collect::<Vec<&str>>();
+    let mut sum: u32 = 0;
+    for line in 0..lines.len()/3 {
+        let matched_char: char = lines[line*3].chars().find(|c| lines[line*3+1].contains(*c) && lines[line*3+2].contains(*c)).unwrap();
+        sum += find_value(matched_char);
+    }
+    sum
+}
 
 fn main() {
-    println!("Hello, world!");
-    let data = read_data("input.txt");
-    let mut matched_chars: Vec<u32> = Vec::new();
-    data.split_terminator("\n").for_each( |line| {
-        println!("Line: {}", line);
-        println!("Line length: {}", line.len());
-        println!("first part: {}", &line[0..(line.len()/2)]);
-        println!("second part: {}", &line[(line.len()/2)..]);
-        for c in line[0..line.len()/2].chars() {
-            if line[line.len()/2..].find(c).is_some() {
-                println!("Found match: {}", c);
-                let char_value: u32; 
-                if c.is_uppercase() {
-                    char_value = 26+(c.to_digit(36).unwrap()-9);
-                } else {
-                    char_value = (c.to_digit(36).unwrap()-9).try_into().unwrap();
-                }
-                println!("Value: {}", char_value);
-                matched_chars.push(char_value);
-                break;
-            }
-        };
-    });
-    let lines: Vec<&str> = data.split_terminator("\n").collect();
-    let mut matched_badges: Vec<u32> = Vec::new();
-    for i in 0..lines.len()/3 {
-        for c in lines[(i)*3].chars() {
-            if lines[(i)*3+1].find(c).is_some() {
-                if lines[(i)*3+2].find(c).is_some() {
-                    let char_value: u32;
-                    if c.is_uppercase() {
-                        char_value = 26+(c.to_digit(36).unwrap()-9);
-                    } else {
-                        char_value = c.to_digit(36).unwrap()-9;
-                    }
-                    matched_badges.push(char_value);
-                    break;
-                }
-            }
-        }
-    }
-    let sum = matched_chars.iter().sum::<u32>();
-    println!("Sum: {}", sum);
-    let sum_badge = matched_badges.iter().sum::<u32>();
-    println!("Sum badge: {}", sum_badge);
+    let data: String = read_to_string("input.txt").expect("Error reading file");
+    println!("Sum: {}", part1(&data));
+    println!("Sum badge: {}", part2(&data));
 }
